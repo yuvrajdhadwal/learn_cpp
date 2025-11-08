0f7b57092a101368c5a5e917717b37a6864c482f
Yuvraj Dhadwal <ydhadwal3@gatech.edu>
2025-11-08 13:45:34 -0500

    variable initialization

---
+++ 1. C++ Basics/VariableInitialization.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    // modern, recommended to do
+    [[maybe_unused]] int x;     // default-initialization (no initializer) - garbage value
+    // [[maybe_unused]] attribute tells the compiler a variable is unused and it is fine. 
+    // The compiler will not generate unused variable warnings for such variables.
+
+    int y { 5 };                // direct-list-initialization (initial value in braces)
+    // forbids narrowing conversions 4.5 causes error instead of turning into 4
+    int e {};                   // value-initialization (empty braces) - defaults to 0
+
+    // traditional, do not do
+    [[maybe_unused]] int b = 5;      // copy-initialization (initial value after equals sign)
+    // also used when pass by value into functions
+    [[maybe_unused]] int c ( 6 );    // direct-initialization (initial value in parenthesis)
+
+    std::cout << y;
+    std::cout << e;
+
+    return 0;
+}
